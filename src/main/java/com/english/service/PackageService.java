0637e61cbf0df72bdf30735d4d35a169be2b6ca6
KongTien2004 <22130131@st.hcmauf.edu.vn>
2025-12-07 16:19:53 +0700

    feat (service): Define methods handling queries in PackageDAO

--- src/main/java/com/english/service/PackageService.java
+++ src/main/java/com/english/service/PackageService.java
@@ -1,4 +1,53 @@
 package com.english.service;
 
+import com.english.DAO.PackageDAO;
+import com.english.model.Package;
+
+import java.util.List;
+
 public class PackageService {
+    private PackageDAO packageDAO;
+
+    public PackageService(PackageDAO packageDAO) {
+        this.packageDAO = packageDAO;
+    }
+
+    public boolean insertPackage(Package aPackage) {
+        if (!validatePacakge(aPackage)) return false;
+        return packageDAO.insertPackage(aPackage);
+    }
+
+    public boolean updatePackage(Package aPackage) {
+        if (!validatePacakge(aPackage)) return false;
+        return packageDAO.updatePackage(aPackage);
+    }
+
+    public boolean deletePackage(Package aPackage) {
+        if (!validatePacakge(aPackage)) return false;
+        return packageDAO.deletePackage(aPackage);
+    }
+
+    public List<Package> getAllPackages() {
+        return packageDAO.getAllPackages();
+    }
+
+    public Package getPackageById(String packageId) {
+        return packageDAO.getPackageById(packageId);
+    }
+
+    private boolean validatePacakge(Package aPackage) {
+        if (aPackage == null) return false;
+        if (aPackage.getPackageId() == null || aPackage.getPackageId().trim().isEmpty()) return false;
+        if (aPackage.getPackageName() == null || aPackage.getPackageName().trim().isEmpty()) return false;
+        if (aPackage.getIeltsType() == null) return false;
+        if (aPackage.getTargetBand() <= 0) return false;
+        if (aPackage.getTotalSessions() <= 0) return false;
+        if (aPackage.getPrice() <= 0) return false;
+
+        return true;
+    }
+
+    public int totalPackages() {
+        return packageDAO.getAllPackages().size();
+    }
 }
