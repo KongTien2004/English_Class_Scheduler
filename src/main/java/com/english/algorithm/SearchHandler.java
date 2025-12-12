4218e96405a661e4a47efefa2fd4324c142164e9
KongTien2004 <22130131@st.hcmauf.edu.vn>
2025-12-12 16:38:21 +0700

    feat (algorithm): Add LearningSession entity searching handler

--- src/main/java/com/english/algorithm/SearchHandler.java
+++ src/main/java/com/english/algorithm/SearchHandler.java
@@ -1,9 +1,11 @@
 package com.english.algorithm;
 
 import com.english.algorithm.strategy.CenterSearchStrategy;
+import com.english.algorithm.strategy.LearningSessionSearchStrategy;
 import com.english.algorithm.strategy.MentorSearchStrategy;
 import com.english.algorithm.strategy.StudentSearchStrategy;
 import com.english.model.Center;
+import com.english.model.LearningSession;
 import com.english.model.Mentor;
 import com.english.model.Student;
 
@@ -18,5 +20,9 @@ public class SearchHandler {
 
     public static OptimizedSearchHandler<Student> studentHandler() {
         return new OptimizedSearchHandler<>(new StudentSearchStrategy());
+    }
+
+    public static OptimizedSearchHandler<LearningSession> learningSessionHandler() {
+        return new OptimizedSearchHandler<>(new LearningSessionSearchStrategy());
     }
 }
