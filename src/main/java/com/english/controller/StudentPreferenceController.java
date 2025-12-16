d0beb25aa0b212961f387739c645d9a739ba09c4
KongTien2004 <22130131@st.hcmauf.edu.vn>
2025-12-16 10:51:46 +0700

    feat (controller): Define controller of student's preferences using CRUD and UI response

--- src/main/java/com/english/controller/StudentPreferenceController.java
+++ src/main/java/com/english/controller/StudentPreferenceController.java
@@ -1,4 +1,76 @@
 package com.english.controller;
 
+import com.english.model.StudentPreference;
+import com.english.service.StudentPreferenceService;
+
+import javax.swing.*;
+import java.util.List;
+
 public class StudentPreferenceController {
+    private StudentPreferenceService studentPreferenceService;
+
+    public StudentPreferenceController(StudentPreferenceService studentPreferenceService) {
+        this.studentPreferenceService = studentPreferenceService;
+    }
+
+    public void insertPreference(StudentPreference studentPreference, JFrame frame) {
+        if (studentPreferenceService.insertPreference(studentPreference)) {
+            JOptionPane.showMessageDialog(frame,
+                    "Student's preference has been inserted!",
+                    "Successfully",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(frame,
+                    "Please check the information",
+                    "Failure",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void updatePreference(StudentPreference studentPreference, JFrame frame) {
+        if (studentPreferenceService.updatePreference(studentPreference)) {
+            JOptionPane.showMessageDialog(frame,
+                    "Student's preference has been updated!",
+                    "Successfully",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(frame,
+                    "Please check the information",
+                    "Failure",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void deletePreference(StudentPreference studentPreference, JFrame frame) {
+        int confirm = JOptionPane.showConfirmDialog(frame,
+                "Are you sure to delete it?",
+                "Deleting confirm",
+                JOptionPane.YES_NO_OPTION);
+
+        if (confirm == JOptionPane.YES_OPTION) {
+            if (studentPreferenceService.deletePreference(studentPreference)) {
+                JOptionPane.showMessageDialog(frame,
+                        "Student's preference has been deleted!",
+                        "Successfully",
+                        JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(frame,
+                        "Please check the information",
+                        "Failure",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public StudentPreference getPreferenceById(String preferenceId) {
+        return studentPreferenceService.getPreferenceById(preferenceId);
+    }
+
+    public List<StudentPreference> getAllPreferences() {
+        return studentPreferenceService.getAllPreferences();
+    }
+
+    public List<StudentPreference> getPreferenceByStudent(String studentId) {
+        return studentPreferenceService.getPreferenceByStudent(studentId);
+    }
 }
