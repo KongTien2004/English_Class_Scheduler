dc6cca3bee5dc5752ee7931cb834d947438c9eb9
KongTien2004 <22130131@st.hcmauf.edu.vn>
2025-12-16 10:49:26 +0700

    feat (service): Define methods handling queries in DAO class

--- src/main/java/com/english/service/StudentPreferenceService.java
+++ src/main/java/com/english/service/StudentPreferenceService.java
@@ -1,4 +1,38 @@
 package com.english.service;
 
+import com.english.DAO.StudentPreferenceDAO;
+import com.english.model.StudentPreference;
+
+import java.util.List;
+
 public class StudentPreferenceService {
+    private StudentPreferenceDAO studentPreferenceDAO;
+
+    public StudentPreferenceService(StudentPreferenceDAO studentPreferenceDAO) {
+        this.studentPreferenceDAO = studentPreferenceDAO;
+    }
+
+    public boolean insertPreference(StudentPreference studentPreference) {
+        return studentPreferenceDAO.insertStudentPreference(studentPreference);
+    }
+
+    public boolean updatePreference(StudentPreference studentPreference) {
+        return studentPreferenceDAO.updateStudentPreference(studentPreference);
+    }
+
+    public boolean deletePreference(StudentPreference studentPreference) {
+        return studentPreferenceDAO.deleteStudentPreference(studentPreference);
+    }
+
+    public StudentPreference getPreferenceById(String preferenceId) {
+        return studentPreferenceDAO.getStudentPreferenceById(preferenceId);
+    }
+
+    public List<StudentPreference> getAllPreferences() {
+        return studentPreferenceDAO.getAllPreferences();
+    }
+
+    public List<StudentPreference> getPreferenceByStudent(String studentId) {
+        return studentPreferenceDAO.getPreferenceByStudent(studentId);
+    }
 }
