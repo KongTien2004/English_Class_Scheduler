b922ca4c0cfa6412eee06b7bae968eecd11656f5
KongTien2004 <22130131@st.hcmuaf.edu.vn>
2026-01-05 02:10:03 +0700

    refactor (algorithm): Fix the data type

--- src/main/java/com/english/algorithm/Scheduler.java
+++ src/main/java/com/english/algorithm/Scheduler.java
@@ -543,8 +543,8 @@ public class Scheduler {
         session.setSessionNumber(sessionNumber);
         session.setSessionType(proposal.sessionType);
         session.setScheduledTime(proposal.scheduledTime);
-        session.setStartTime(proposal.scheduledTime);
-        session.setEndTime(endTime);
+        session.setStartTime(proposal.scheduledTime.toLocalTime());
+        session.setEndTime(endTime.toLocalTime());
         session.setLocation(proposal.centerId);
         session.setSessionStatus(LearningSession.SessionStatus.scheduled);
 
